848975076177f186b4feb52464aae45b68571285
LuanCLF <129459306+LuanCLF@users.noreply.github.com>
2023-08-19 17:10:00 -0300

    Delete exercicios-backend-arrays-loops directory

--- exercicios-backend-arrays-loops/04/index.js
+++
@@ -1,5 +0,0 @@
-for (let i = 0; i <= 500; i++) {
-  if (i % 3 === 0) {
-    console.log(i);
-  }
-}
--- exercicios-backend-arrays-loops/09/index.js
+++
@@ -1,8 +0,0 @@
-const filaDePedidos = ["pedido 12", "pedido 13", "pedido 14"];
-
-for (let pedido of filaDePedidos) {
-  filaDePedidos.push(`pedido 15`);
-  filaDePedidos.shift();
-  break;
-}
-console.log(filaDePedidos);
